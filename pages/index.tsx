4db6dbe59cafed7903142021ac0cbfb0b53ab70e
LarsAssen <juniorassen@hotmail.com>
2024-05-16 10:01:14 +0200

    changed hero jpg

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
 
   return (
     <Layout>
-      <Hero title={"My Blog"} text={"Welcome to my blog. I hope you enjoy it."} imageUrl="https://blog-lars-backend-pjobs.ondigitalocean.app/uploads/hero_bf0f08bdb3.jpg" primaryButtonText={"Blog"} secondaryButtonText={"Newsletter"} primaryButtonOnClick={handlePrimaryButtonClick} secondaryButtonOnClick={handleSecondaryButtonClick}/>
+      <Hero title={"My Blog"} text={"Welcome to my blog. I hope you enjoy it."} imageUrl="https://blog-lars-backend-pjobs.ondigitalocean.app/uploads/hero_163b561477.jpg" primaryButtonText={"Blog"} secondaryButtonText={"Newsletter"} primaryButtonOnClick={handlePrimaryButtonClick} secondaryButtonOnClick={handleSecondaryButtonClick}/>
       <BlogPostList limit={4} />
       <div ref={newsletterRef} >
       <NewsletterForm />
