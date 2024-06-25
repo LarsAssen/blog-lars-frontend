36017405055a18abdeb7991e5c78e1c7c71154d3
LarsAssen <juniorassen@hotmail.com>
2024-06-25 16:34:35 +0200

    view size fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/icon.ico" type="image/x-icon"/>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
         </Head>
         <body>
           <Main />
