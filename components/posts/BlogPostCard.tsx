b4db30daa004c8e8f10b861d25f1a2ae2456cdb9
LarsAssen <juniorassen@hotmail.com>
2024-07-25 20:42:08 +0200

    start for animations

--- components/posts/BlogPostCard.tsx
+++ components/posts/BlogPostCard.tsx
@@ -5,37 +5,58 @@ import Link from "next/link";
 import styled from "styled-components";
 import Title from "../UI/Title";
 import Button from "../UI/Button";
+import { animated, useSpring } from '@react-spring/web'
+import { useInView } from 'react-intersection-observer'
 
 
 const BlogPostCard:React.FC<{post:Post}> = ({post}) => {
+    const [ref, inView] = useInView({
+        triggerOnce: false,
+        threshold: 0.1,
+      });
+
+
+    const styles = useSpring({
+        from: {
+          opacity: 0,
+          y: '6%',
+        },
+        to: {
+          opacity: 1,
+          y: '0%',
+        },
+      })
+
     return (
-        <Card>
-            <CardImage src={post.HeaderImage} />
-            <CardContent>
-                <Header>
-                <CategoryPill>
-                        <CategoryIcon />
-                        {post.category}
-                </CategoryPill>
-                    
-                    <TimeToRead>{post.ReadTime} min read</TimeToRead>
-                    <Title level={5}>{post.Title}</Title>
-                </Header>
-                <CardDescription> {post.Description.length > 100 ? `${post.Description.substring(0, 100)}...` : post.Description}</CardDescription>
-                <DateText>{format(new Date(post.publishedAt), 'd MMM yyyy')}</DateText>
-                <Link href={`posts/${post.Slug}`} passHref>
-                <Button>Read More</Button>
-                </Link>
-                <TagsContainer>
-                    {post.tags.map((tag, index) => (
-                        <TagPill key={index}>
-                        <TagIcon />
-                        {tag}
-                        </TagPill>
-                    ))}
-                </TagsContainer>
-            </CardContent>
-        </Card>
+        <animated.div style={styles} >
+            <Card>
+                <CardImage src={post.HeaderImage} />
+                <CardContent>
+                    <Header>
+                    <CategoryPill>
+                            <CategoryIcon />
+                            {post.category}
+                    </CategoryPill>
+                        
+                        <TimeToRead>{post.ReadTime} min read</TimeToRead>
+                        <Title level={5}>{post.Title}</Title>
+                    </Header>
+                    <CardDescription> {post.Description.length > 100 ? `${post.Description.substring(0, 100)}...` : post.Description}</CardDescription>
+                    <DateText>{format(new Date(post.publishedAt), 'd MMM yyyy')}</DateText>
+                    <Link href={`posts/${post.Slug}`} passHref>
+                    <Button>Read More</Button>
+                    </Link>
+                    <TagsContainer>
+                        {post.tags.map((tag, index) => (
+                            <TagPill key={index}>
+                            <TagIcon />
+                            {tag}
+                            </TagPill>
+                        ))}
+                    </TagsContainer>
+                </CardContent>
+            </Card>
+        </animated.div>
     );
 }
 
