6206b8726f7938528ddf9968045e740e83e20cfd
LarsAssen <juniorassen@hotmail.com>
2024-09-22 16:25:32 +0200

    fully updated blogpost card design

--- components/posts/BlogPostCard/BlogPostCard.tsx
+++ components/posts/BlogPostCard/BlogPostCard.tsx
@@ -1,60 +1,54 @@
-import {
-  Card,
-  CardButton,
-  CardContent,
-  CardDescription,
-  CardImage,
-  CardTitle,
-  CategoryIcon,
-  CategoryPill,
-  DateText,
-  Header,
-  TagIcon,
-  TagPill,
-  TagsContainer,
-  TimeToRead,
-} from "@/styles/Posts/BlogPostCardStyles";
 import { Post } from "@/types";
 import { format } from "date-fns";
 import Link from "next/link";
-import styled from "styled-components";
 import Title from "../../UI/Title";
 import Button from "../../UI/Button";
+import styles from "./BlogPostCard.module.scss";
+import { Calendar, Clock, ArrowRight } from "react-feather";
 
 const BlogPostCard: React.FC<{ post: Post }> = ({ post }) => {
   return (
-    <Card>
-      <CardImage src={post.HeaderImage} />
-      <CardContent>
-        <Header>
-          <CategoryPill>
-            <CategoryIcon />
-            {post.category}
-          </CategoryPill>
-
-          <TimeToRead>{post.ReadTime} min read</TimeToRead>
-          <Title level={5}>{post.Title}</Title>
-        </Header>
-        <CardDescription>
-          {" "}
-          {post.Description.length > 100
-            ? `${post.Description.substring(0, 100)}...`
-            : post.Description}
-        </CardDescription>
-        <DateText>{format(new Date(post.publishedAt), "d MMM yyyy")}</DateText>
-        <Link href={`posts/${post.Slug}`} passHref>
-          <Button>Read More</Button>
-        </Link>
-        <TagsContainer>
-          {post.tags.map((tag, index) => (
-            <TagPill key={index}>
-              <TagIcon />
-              {tag}
-            </TagPill>
-          ))}
-        </TagsContainer>
-      </CardContent>
-    </Card>
+    <div className={styles.card}>
+      <a href={`/blog/${post.Slug}`} className={styles.link}>
+        <div className={styles.imageWrapper}>
+          <img
+            src={post.HeaderImage}
+            alt={post.Title}
+            className={styles.image}
+          />
+          <span className={styles.categoryBadge}>{post.category}</span>
+        </div>
+        <div className={styles.cardHeader}>
+          <div className={styles.headerInfo}>
+            <div className={styles.date}>
+              <Calendar className={styles.icon} />
+              {new Date(post.publishedAt).toLocaleDateString()}
+            </div>
+            <div className={styles.readingTime}>
+              <Clock className={styles.icon} />
+              {post.ReadTime} min read
+            </div>
+          </div>
+          <h3 className={styles.title}>{post.Title}</h3>
+          <p className={styles.description}>{post.Description}</p>
+        </div>
+        <div className={styles.cardContent}>
+          <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <span key={tag} className={styles.tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className={styles.cardFooter}>
+          <span className={styles.readMore}>
+            Read More
+            <ArrowRight className={styles.readMoreIcon} />
+          </span>
+        </div>
+      </a>
+    </div>
   );
 };
 
